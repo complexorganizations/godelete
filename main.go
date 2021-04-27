be9b8a31d0989d2e9939d1d6c893915363c1f287
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-04-27 08:25:49 -0400

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,56 @@ import (
 	"fmt"
 )
 
+var (
+	goUserPath = fmt.Sprint(userDirectory() + "/go")
+	goUserBinPath = fmt.Sprint(goUserPath + "/bin")
+)
+
+func init() {
+	// System Requirements Check
+	if !commandExists("go") {
+		log.Fatal("Error: The application go was not found in the system.")
+	}
+}
+
 func main() {
 	fmt.Println("Hello, playground")
+}
+
+
+// Check if a folder exists
+func folderExists(foldername string) bool {
+	info, err := os.Stat(foldername)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return info.IsDir()
+}
+
+// check if a file exists
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+// Check if there is an app installed
+func commandExists(cmd string) bool {
+	cmd, err := exec.LookPath(cmd)
+	if err != nil {
+		return false
+	}
+	_ = cmd // variable declared and not used
+	return true
+}
+
+// Get the current user dir
+func userDirectory() string {
+	user, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return user.HomeDir
 }
